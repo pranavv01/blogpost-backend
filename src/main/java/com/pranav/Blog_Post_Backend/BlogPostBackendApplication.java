bec66daeeb89fd92abe735dcfa8ded068f4f13e5
pranavv01 <anand.pranav26@gmail.com>
2025-06-11 14:39:00 +0530

    Initial Commit

---
+++ src/main/java/com/pranav/Blog_Post_Backend/BlogPostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.Blog_Post_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pranav/Blog_Post_Backend/BlogPostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.Blog_Post_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
