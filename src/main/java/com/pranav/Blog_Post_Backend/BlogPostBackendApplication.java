64bce989834118a63faa7c2e9fc26b198b0e85fc
pranavv01 <anand.pranav26@gmail.com>
2025-06-11 14:42:12 +0530

    Initial Commit

--- src/main/java/com/pranav/Blog_Post_Backend/BlogPostBackendApplication.java
+++ src/main/java/com/pranav/Blog_Post_Backend/BlogPostBackendApplication.java
@@ -2,12 +2,13 @@ package com.pranav.Blog_Post_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BlogPostBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogPostBackendApplication.class, args);
+		ApplicationContext context = SpringApplication.run(BlogPostBackendApplication.class, args);
 	}
 
 }
